f95b6d579348754d7fedc6a0aa60f5f23e0eb4c4
Adrian <adrian.richter@gmail.com>
2020-03-27 14:57:47 +0800

    MAde the return types explicit

--- src/main/kotlin/com/github/squirrelgrip/extensions/Time.kt
+++ src/main/kotlin/com/github/squirrelgrip/extensions/Time.kt
@@ -3,10 +3,10 @@ package com.github.squirrelgrip.extensions
 import java.time.*
 import java.util.*
 
-inline fun Instant.toDate() = Date(this.toEpochMilli())
+inline fun Instant.toDate(): Date = Date(this.toEpochMilli())
 
-inline fun LocalDateTime.toDate(offset: ZoneOffset = ZoneOffset.UTC) = this.toInstant(offset)
+inline fun LocalDateTime.toDate(offset: ZoneOffset = ZoneOffset.UTC): Date = this.toInstant(offset).toDate()
 
-inline fun OffsetDateTime.toDate() = this.toInstant().toDate()
+inline fun OffsetDateTime.toDate(): Date = this.toInstant().toDate()
 
-inline fun ZonedDateTime.toDate() = this.toInstant().toDate()
+inline fun ZonedDateTime.toDate(): Date = this.toInstant().toDate()
