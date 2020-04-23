c3878fa2bdb3bb925a62787699490221273c6a63
Adrian <adrian.richter@gmail.com>
2020-04-23 08:58:27 +0800

    Renamed reverse to swap

--- src/main/kotlin/com/github/squirrelgrip/extensions/map/MapExtensions.kt
+++ src/main/kotlin/com/github/squirrelgrip/extensions/map/MapExtensions.kt
@@ -4,7 +4,7 @@ package com.github.squirrelgrip.extensions.map
  * Reverses a List<Pair<K, V>> into a Map<V, List<K>>
  * @return reversed Map<V, List<K>>
  */
-fun <K, V> Iterable<Pair<K, V>>.reverse(): Map<V, List<K>> =
+fun <K, V> Iterable<Pair<K, V>>.swap(): Map<V, List<K>> =
     groupBy { it.second }.mapValues { (_, value) ->
         value.map {
             it.first
@@ -15,14 +15,14 @@ fun <K, V> Iterable<Pair<K, V>>.reverse(): Map<V, List<K>> =
  * Reverses a map of K to V into a map of V to List<K>
  * @return reversed Map<V, List<K>>
  */
-fun <K, V> Map<K, V>.reverse(): Map<V, List<K>> =
-    toList().reverse()
+fun <K, V> Map<K, V>.swap(): Map<V, List<K>> =
+    toList().swap()
 
 /**
  * Reverses a map of K to Iterable<V> into a map of V to List<K>
  * @return reversed Map<V, List<K>>
  */
-fun <K, V> Map<K, Iterable<V>>.reverseWithCollection(): Map<V, List<K>> =
+fun <K, V> Map<K, Iterable<V>>.swapWithCollection(): Map<V, List<K>> =
     entries.flatMap { e ->
         e.value.map { e.key to it }
-    }.reverse()
+    }.swap()
