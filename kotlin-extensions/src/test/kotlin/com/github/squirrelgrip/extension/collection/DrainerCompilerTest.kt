cac83287b61389d897dcd98a9a785932e7a9cefe
Adrian Richter <adrian.richter@gmail.com>
2022-04-16 23:04:00 +0800

    Updating the dependencies

--- kotlin-extensions/src/test/kotlin/com/github/squirrelgrip/extension/collection/DrainerCompilerTest.kt
+++ kotlin-extensions/src/test/kotlin/com/github/squirrelgrip/extension/collection/DrainerCompilerTest.kt
@@ -9,13 +9,21 @@ internal class DrainerCompilerTest {
     val setOfA = setOf("A")
     val setOfB = setOf("B")
     val setOfC = setOf("C")
-    val setOfAB = setOf("A", "B")
+    val setOfAAndB = setOf("A", "B")
+    val setOfAA = setOf("AA")
+    val setOfAB = setOf("AB")
+    val setOfAC = setOf("AC")
+    val setOfAAAndAB = setOf("AA", "AB")
 
     val collection = listOf(
         1 to setOfA,
         2 to setOfB,
         3 to setOfC,
-        4 to setOfAB
+        4 to setOfAAndB,
+        5 to setOfAA,
+        6 to setOfAB,
+        7 to setOfAC,
+        8 to setOfAAAndAB,
     )
 
     @Test
@@ -25,7 +33,7 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isTrue
         assertThat(compile.invoke(setOfB)).isFalse
         assertThat(compile.invoke(setOfC)).isFalse
-        assertThat(compile.invoke(setOfAB)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
 
         assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(1, 4)
     }
@@ -37,7 +45,7 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isTrue
         assertThat(compile.invoke(setOfB)).isFalse
         assertThat(compile.invoke(setOfC)).isFalse
-        assertThat(compile.invoke(setOfAB)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
 
         assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(1, 4)
     }
@@ -49,9 +57,9 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isFalse
         assertThat(compile.invoke(setOfB)).isTrue
         assertThat(compile.invoke(setOfC)).isTrue
-        assertThat(compile.invoke(setOfAB)).isFalse
+        assertThat(compile.invoke(setOfAAndB)).isFalse
 
-        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(2, 3)
+        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(2, 3, 5, 6, 7, 8)
     }
 
     @Test
@@ -61,9 +69,9 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isFalse
         assertThat(compile.invoke(setOfB)).isTrue
         assertThat(compile.invoke(setOfC)).isTrue
-        assertThat(compile.invoke(setOfAB)).isFalse
+        assertThat(compile.invoke(setOfAAndB)).isFalse
 
-        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(2, 3)
+        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(2, 3, 5, 6, 7, 8)
     }
 
     @Test
@@ -73,7 +81,7 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isTrue
         assertThat(compile.invoke(setOfB)).isTrue
         assertThat(compile.invoke(setOfC)).isFalse
-        assertThat(compile.invoke(setOfAB)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
 
         assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(1, 2, 4)
     }
@@ -85,7 +93,7 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isFalse
         assertThat(compile.invoke(setOfB)).isFalse
         assertThat(compile.invoke(setOfC)).isFalse
-        assertThat(compile.invoke(setOfAB)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
 
         assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(4)
     }
@@ -97,9 +105,57 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isTrue
         assertThat(compile.invoke(setOfB)).isFalse
         assertThat(compile.invoke(setOfC)).isTrue
-        assertThat(compile.invoke(setOfAB)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
 
-        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(1, 3, 4)
+        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(1, 3, 4, 5, 6, 7, 8)
+    }
+
+    @Test
+    fun compile_GivenOrNotBQuestionMarkVariable() {
+        val input = "A|!B?"
+        val compile = testSubject.compile(input)
+        assertThat(compile.invoke(setOfA)).isTrue
+        assertThat(compile.invoke(setOfB)).isTrue
+        assertThat(compile.invoke(setOfC)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
+
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(1, 2, 3, 4, 5, 6, 7, 8)
+    }
+
+    @Test
+    fun compile_GivenOrNotAQuestionMarkVariable() {
+        val input = "A|!A?"
+        val compile = testSubject.compile(input)
+        assertThat(compile.invoke(setOfA)).isTrue
+        assertThat(compile.invoke(setOfB)).isTrue
+        assertThat(compile.invoke(setOfC)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
+
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(1, 2, 3, 4)
+    }
+
+    @Test
+    fun compile_GivenOrNotAAsteriskVariable() {
+        val input = "A|!A*"
+        val compile = testSubject.compile(input)
+        assertThat(compile.invoke(setOfA)).isTrue
+        assertThat(compile.invoke(setOfB)).isTrue
+        assertThat(compile.invoke(setOfC)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
+
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(1, 2, 3, 4)
     }
 
     @Test
@@ -109,9 +165,65 @@ internal class DrainerCompilerTest {
         assertThat(compile.invoke(setOfA)).isTrue
         assertThat(compile.invoke(setOfB)).isFalse
         assertThat(compile.invoke(setOfC)).isFalse
-        assertThat(compile.invoke(setOfAB)).isFalse
+        assertThat(compile.invoke(setOfAAndB)).isFalse
 
-        assertThat(collection.filter(input) { it.second }.map { it.first }).containsExactly(1)
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(1)
+    }
+
+    @Test
+    fun compile_GivenAsteriskVariable() {
+        val input = "*"
+        val compile = testSubject.compile(input)
+        assertThat(compile.invoke(setOfA)).isTrue
+        assertThat(compile.invoke(setOfB)).isTrue
+        assertThat(compile.invoke(setOfC)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
+
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(1, 2, 3, 4, 5, 6, 7, 8)
+    }
+
+    @Test
+    fun compile_GivenQuestionMarkVariable() {
+        val input = "?"
+        val compile = testSubject.compile(input)
+        assertThat(compile.invoke(setOfA)).isTrue
+        assertThat(compile.invoke(setOfB)).isTrue
+        assertThat(compile.invoke(setOfC)).isTrue
+        assertThat(compile.invoke(setOfAAndB)).isTrue
+
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(1, 2, 3, 4)
+    }
+
+    @Test
+    fun compile_GivenAQuestionMarkVariable() {
+        val input = "A?"
+        val compile = testSubject.compile(input)
+        assertThat(compile.invoke(setOfA)).isFalse
+        assertThat(compile.invoke(setOfB)).isFalse
+        assertThat(compile.invoke(setOfC)).isFalse
+        assertThat(compile.invoke(setOfAAndB)).isFalse
+        assertThat(compile.invoke(setOfAA)).isTrue
+        assertThat(compile.invoke(setOfAB)).isTrue
+        assertThat(compile.invoke(setOfAC)).isTrue
+        assertThat(compile.invoke(setOfAAAndAB)).isTrue
+
+        assertThat(collection.filter(input) {
+            it.second
+        }.map {
+            it.first
+        }).containsExactly(5, 6, 7, 8)
     }
 
 }
